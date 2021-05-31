33df3108870725879e552868908c1d05b3fadd59
mingqi420 <lidefu_420@163.com>
2021-05-31 09:38:23 +0800

    新增

---
+++ web/src/test/java/com/mingqi/platform/web/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingqi.platform.web.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
